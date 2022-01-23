cf0a5f06bff4a66fcbe1107890707062bb7e1dc7
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-24 00:42:51 +0530

    first-push

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSlider(_ sender: UISlider) {
+        print(sender.value)
+        let height = String(format: "%.2f",sender.value)
+        heightLabel.text = "\(height) m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        print(sender.value)
+        let weight = String(format: "%.0f",sender.value)
+        weightLabel.text = "\(weight) Kg"
+    }
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 }
 
